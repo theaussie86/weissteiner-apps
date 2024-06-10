a8ee7320b1f3b39ca73225c07321e092673973ff
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2024-06-10 07:02:00 +0200

    chore: Update page titles and descriptions for localization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - TaxPal',
-    default: 'TaxPal - Accounting made simple for small businesses',
+    template: '%s - CWeissteiner',
+    default: 'CWeissteiner - Automation für Selbstständige und KMUs',
   },
   description:
     'Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited.',
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,19 +8,19 @@ export default function NotFound() {
   return (
     <SlimLayout>
       <div className="flex">
-        <Link href="/" aria-label="Home">
+        <Link href="/" aria-label="Startseite">
           <Logo className="h-10 w-auto" />
         </Link>
       </div>
       <p className="mt-20 text-sm font-medium text-gray-700">404</p>
       <h1 className="mt-3 text-lg font-semibold text-gray-900">
-        Page not found
+        Seite nicht gefunden
       </h1>
       <p className="mt-3 text-sm text-gray-700">
-        Sorry, we couldn’t find the page you’re looking for.
+        Entschuldigung, wir konnten die gesuchte Seite nicht finden.
       </p>
       <Button href="/" className="mt-10">
-        Go back home
+        Zurück zur Startseite
       </Button>
     </SlimLayout>
   )
