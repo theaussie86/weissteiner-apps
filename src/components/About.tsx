a568972c1f08061d79a47aa601431ba1afd69b4c
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2024-06-13 06:33:28 +0200

    exchanged my picture

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image'
 import React from 'react'
+
+import portrait from '@/images/screenshots/christoph-weissteiner.jpg'
 
 export default function About() {
   return (
@@ -22,9 +25,9 @@ export default function About() {
                 entscheidet.
               </p>
             </div>
-            <img
-              src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
-              alt=""
+            <Image
+              src={portrait}
+              alt="Christoph Weissteiner"
               className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
             />
           </div>
