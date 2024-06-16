51c52c636451ef5f11e2d2acad9223125f869fc0
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2024-06-16 10:23:43 +0200

    feat: Add favicon and update page metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export const metadata: Metadata = {
   },
   description:
     'Entdecke maßgeschneiderte Automatisierungslösungen für Selbstständige und kleine bis mittelständische Unternehmen (KMUs). Optimiere Deine Geschäftsprozesse, spare Zeit und Ressourcen und konzentriere Dich auf das Wesentliche - Dein Geschäftswachstum. Unsere Experten helfen Dir, die richtige Technologie zu finden und nahtlos zu integrieren. Starte jetzt und mache den ersten Schritt in eine effizientere Zukunft!',
+  icons: {
+    icon: `/favicon.ico?v=2`,
+  },
 }
 
 const inter = Inter({
