2925548976032f28c96e7167d8df33e1566981bb
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2024-06-16 10:07:40 +0200

    change cache headers for favicon

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+          },
+        ],
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
